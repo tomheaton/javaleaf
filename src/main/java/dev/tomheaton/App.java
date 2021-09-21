d415f5c41d95968885821defceb957542819c9cf
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-21 14:26:13 +0100

    new things

---
+++ src/main/java/dev/tomheaton/App.java
@@ -0,0 +1,8 @@
+package dev.tomheaton;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.print("hello world - javaleaf");
+    }
+}
---
+++ src/test/java/dev/tomheaton/AppTest.java
@@ -0,0 +1,8 @@
+package dev.tomheaton;
+
+public class AppTest {
+    
+    public static void main(String[] args) {
+        System.out.print("hello world - javaleaf");
+    }
+}
